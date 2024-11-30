2cffc619bd9466bdc598de4aebc8a4bc49b51e43
Left-back <s23C1027AG@s.chibakoudai.jp>
2024-11-30 12:06:21 +0900

    Add tests

--- test.bash
+++ test.bash
@@ -1,7 +1,27 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Leftback
+# SPDX-License-Identifier: BSD-3-Clause
 
+
+ng () {
+    echo NG at Line $1
+    res=1
+}
+
+res=0
+
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng $LINENO
 
-[ "${out}" = 15 ]
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)           # 計算できない値を入力してみる
+[ "$?" = 1 ]      || ng $LINENO   # 終了ステータスが1なのを確認
+[ "${out}" = "" ] || ng $LINENO   # この行と上の行は入れ替えるとダメです
+                                  # （↑なぜかは考えてみましょう）
+out=$(echo | ./plus)              # なにも入力しない
+[ "$?" = 1 ]      || ng $LINENO   # これも異常終了する
+[ "${out}" = "" ] || ng $LINENO
 
+[ "$res" = 0 ] && echo OK
+exit $res
