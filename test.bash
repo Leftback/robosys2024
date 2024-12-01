a5912d272db38fded9e08a14de5b6533d71e3a63
Left-back <s23C1027AG@s.chibakoudai.jp>
2024-12-02 01:59:35 +0900

    Fix test

--- test.bash
+++ test.bash
@@ -10,17 +10,28 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-out=$(seq 5 | ./statistics)
-[ "${out}" = 15 ] || ng $LINENO
+out=$(echo "1,2,3,4,5" | ./statistics)
+[ "${out}" = "平均値: 3.0
+最頻値: No unique mode
+中央値: 3
+最大値: 5
+最小値: 1
+範囲: 4
+第一四分位数: 1.75
+第二四分位数（中央値）: 3
+第三四分位数: 4.25
+四分位偏差: 1.25
+分散: 2.5
+標準偏差: 1.5811388300841898" ] || ng $LINENO
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./statistics)
 [ "$?" = 1 ]      || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "${out}" = "入力エラー: invalid literal for int() with base 10: 'あ'" ] || ng $LINENO
 
 out=$(echo | ./statistics)
 [ "$?" = 1 ]      || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "${out}" = "入力エラー: 数列を入力" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
