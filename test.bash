02a7bf8555a1d91164defcffab4bf005c7d4c682
Left-back <s23C1027AG@s.chibakoudai.jp>
2024-12-02 15:47:23 +0900

    rename

--- test.bash
+++ test.bash
@@ -10,28 +10,27 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-out=$(echo "1,2,3,4,5" | python3 statistics_with_test.py)
-expected="平均値: 3
-最頻値: No unique mode
-中央値: 3
+out=$(echo "1,2,3,4,5" | python3 statistics_command.py)
+expected="平均値: 3.0
+中央値: 3.0
 最大値: 5
 最小値: 1
-範囲: 4
+範囲: 4.0
 第一四分位数: 1.5
-第二四分位数（中央値）: 3
+第二四分位数（中央値）: 3.0
 第三四分位数: 4.5
 四分位偏差: 1.5
 分散: 2.5
-標準偏差: 1.58"
+標準偏差: 1.581"
 
 if [ "$out" != "$expected" ]; then
-    echo "Expected:\n$expected"
-    echo "Got:\n$out"
+    echo -e "Expected:\n$expected"
+    echo -e "Got:\n$out"
     ng $LINENO
 fi
 
 ### STRANGE INPUT ###
-out=$(echo "あ" | python3 statistics_with_test.py 2>&1)
+out=$(echo "あ" | python3 statistics_command.py 2>&1)
 if [ "$?" -ne 1 ]; then
     ng $LINENO
 fi
@@ -39,7 +38,7 @@ if [[ "$out" != *"入力エラー: 数列はカンマ区切りの整数で入力
     ng $LINENO
 fi
 
-out=$(echo "" | python3 statistics_with_test.py 2>&1)
+out=$(echo "" | python3 statistics_command.py 2>&1)
 if [ "$?" -ne 1 ]; then
     ng $LINENO
 fi
