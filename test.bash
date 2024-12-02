6d4164b71ea911fc73751c3257c7bd1d07dd75ef
Left-back <s23C1027AG@s.chibakoudai.jp>
2024-12-03 01:09:15 +0900

    Fix

--- test.bash
+++ test.bash
@@ -10,42 +10,28 @@ ng () {
 res=0
 
 ### 正常な入力 ###
-out=$(echo "1,2,3,4,5" | python3 statistics.py)
-expected="平均値: 3.0
-最頻値: No unique mode
-中央値: 3.0
-最大値: 5.0
-最小値: 1.0
-範囲: 4.0
-第一四分位数: 2.0
-第二四分位数（中央値）: 3.0
-第三四分位数: 4.0
-四分位偏差: 2.0
-分散: 2.0
-標準偏差: 1.4142135623730951"
-
-if [ "$out" != "$expected" ]; then
-    echo -e "Expected:\n$expected"
-    echo -e "Got:\n$out"
-    ng $LINENO
-fi
+out=$(echo "1,2,3,4,5" | python3 calculate_stats.py)
+[[ "$out" == *"平均値: 3.0"* ]] || ng "$LINENO"
+[[ "$out" == *"最頻値: No unique mode"* ]] || ng "$LINENO"
+[[ "$out" == *"中央値: 3.0"* ]] || ng "$LINENO"
+[[ "$out" == *"最大値: 5.0"* ]] || ng "$LINENO"
+[[ "$out" == *"最小値: 1.0"* ]] || ng "$LINENO"
+[[ "$out" == *"範囲: 4.0"* ]] || ng "$LINENO"
+[[ "$out" == *"第一四分位数: 2.0"* ]] || ng "$LINENO"
+[[ "$out" == *"第二四分位数（中央値）: 3.0"* ]] || ng "$LINENO"
+[[ "$out" == *"第三四分位数: 4.0"* ]] || ng "$LINENO"
+[[ "$out" == *"四分位偏差: 2.0"* ]] || ng "$LINENO"
+[[ "$out" == *"分散: 2.0"* ]] || ng "$LINENO"
+[[ "$out" == *"標準偏差: 1.41"* ]] || ng "$LINENO"
 
 ### 異常な入力 ###
-out=$(echo "あ" | python3 statistics.py 2>&1)
-if [ "$?" -ne 1 ]; then
-    ng $LINENO
-fi
-if [[ "$out" != *"could not convert string to float"* ]]; then
-    ng $LINENO
-fi
+out=$(echo "あ" | python3 calculate_stats.py 2>&1)
+[ "$?" -ne 1 ] && ng "$LINENO"
+[[ "$out" == *"could not convert string to float"* ]] || ng "$LINENO"
 
-out=$(echo "" | python3 statistics.py 2>&1)
-if [ "$?" -ne 1 ]; then
-    ng $LINENO
-fi
-if [[ "$out" != *"could not convert string to float"* ]]; then
-    ng $LINENO
-fi
+out=$(echo "" | python3 calculate_stats.py 2>&1)
+[ "$?" -ne 1 ] && ng "$LINENO"
+[[ "$out" == *"could not convert string to float"* ]] || ng "$LINENO"
 
 [ "$res" -eq 0 ] && echo "OK"
 exit $res
