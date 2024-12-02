9829eddb07f02a59feff3327cc5eacb42c1088d5
Left-back <s23C1027AG@s.chibakoudai.jp>
2024-12-02 13:42:15 +0900

    Fix test

--- test.bash
+++ test.bash
@@ -3,15 +3,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-    echo NG at Line $1
+    echo "NG at Line $1"
     res=1
 }
 
 res=0
 
 ### NORMAL INPUT ###
-out=$(echo "1,2,3,4,5" | ./statistics)
-[ "${out}" = "平均値: 3
+out=$(echo "1,2,3,4,5" | python3 statistics_with_test.py)
+expected="平均値: 3
 最頻値: No unique mode
 中央値: 3
 最大値: 5
@@ -22,16 +22,31 @@ out=$(echo "1,2,3,4,5" | ./statistics)
 第三四分位数: 4.5
 四分位偏差: 1.5
 分散: 2.5
-標準偏差: 1.5811388300841898" ] || ng $LINENO
+標準偏差: 1.58"
+
+if [ "$out" != "$expected" ]; then
+    echo "Expected:\n$expected"
+    echo "Got:\n$out"
+    ng $LINENO
+fi
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./statistics)
-[ "$?" = 1 ] || ng $LINENO
-[ "${out}" = "入力エラー: 数列はカンマ区切りの整数で入力してください。" ] || ng $LINENO
+out=$(echo "あ" | python3 statistics_with_test.py 2>&1)
+if [ "$?" -ne 1 ]; then
+    ng $LINENO
+fi
+if [[ "$out" != *"入力エラー: 数列はカンマ区切りの整数で入力してください。"* ]]; then
+    ng $LINENO
+fi
 
-out=$(echo | ./statistics)
-[ "$?" = 1 ] || ng $LINENO
-[ "${out}" = "入力エラー: 数列はカンマ区切りの整数で入力してください。" ] || ng $LINENO
+out=$(echo "" | python3 statistics_with_test.py 2>&1)
+if [ "$?" -ne 1 ]; then
+    ng $LINENO
+fi
+if [[ "$out" != *"入力エラー: 数列はカンマ区切りの整数で入力してください。"* ]]; then
+    ng $LINENO
+fi
 
-[ "$res" = 0 ] && echo OK
+[ "$res" -eq 0 ] && echo "OK"
 exit $res
+
